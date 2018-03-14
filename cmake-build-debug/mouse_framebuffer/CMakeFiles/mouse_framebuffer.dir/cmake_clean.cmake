5e67fbca7ec6886b9dbfffa227bff1e66ca4469d
Scott Goerzen <sgoerzen@uvic.ca>
2018-03-14 10:42:13 -0700

    Added starter code from labs

---
+++ cmake-build-debug/mouse_framebuffer/CMakeFiles/mouse_framebuffer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mouse_framebuffer.dir/main.cpp.o"
+  "mouse_framebuffer.pdb"
+  "mouse_framebuffer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mouse_framebuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/mouse_framebuffer/CMakeFiles/mouse_framebuffer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mouse_framebuffer.
+# This may be replaced when dependencies are built.
